632c00bc4e6c65cc3140debc035eaa1e313e7e78
hbsnow <yuki.4uing@gmail.com>
2019-06-04 23:38:40 +0900

    小数点を含む結果のテスト追加

--- test/common/lerp.spec.js
+++ test/common/lerp.spec.js
@@ -2,6 +2,7 @@ import { lerp } from '@/common/lerp'
 
 describe('lerp', () => {
   test('2点間の距離を求める', () => {
+    expect(lerp([0, 0], [1, 2])).toBe(2.236068)
     expect(lerp([0, 0], [3, 4])).toBe(5)
   })
 })
