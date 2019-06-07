af8c2ece96b793df14fc7b42baebbc38714d3eca
hbsnow <yuki.4uing@gmail.com>
2019-06-08 00:23:37 +0900

    座標上の移動用関数の追加

--- common/map.js
+++ common/map.js
@@ -7,8 +7,26 @@ export const parseLocation = (location) => parseFloat(location.toFixed(7))
 
 /**
  * 二点間の距離を求める
- * @param {number} a
- * @param {number} b
+ * @param {number[]} a 始点
+ * @param {number[]} b 終点
  */
 export const lerp = (a, b) =>
   parseLocation(Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2))
+
+/**
+ * 始点から終点まで指定距離を移動したときの座標と、終点までの残り距離を求める
+ * @param {number[]} a 始点
+ * @param {number[]} b 終点
+ * @param {number} distance 移動距離
+ */
+export const move = (a, b, distance) => {
+  const length = lerp(a, b)
+  if (distance >= length) {
+    return b
+  }
+
+  return [
+    parseLocation((a[0] * (length - distance) + b[0] * distance) / length),
+    parseLocation((a[1] * (length - distance) + b[1] * distance) / length)
+  ]
+}
--- test/common/map.spec.js
+++ test/common/map.spec.js
@@ -1,4 +1,4 @@
-import { parseLocation, lerp } from '@/common/map'
+import { parseLocation, lerp, move } from '@/common/map'
 
 describe('parseLocation', () => {
   test('7桁の小数に変換する', () => {
@@ -14,5 +14,12 @@ describe('lerp', () => {
     expect(lerp([0, 0], [1, 2])).toBe(2.236068)
     expect(lerp([0, 0], [1, 2])).not.toBe(2.236067)
     expect(lerp([0, 0], [3, 4])).toBe(5)
+  })
+})
+
+describe('move', () => {
+  test('指定距離まで移動したときの座標', () => {
+    expect(move([3, 4], [0, 0], 1)).toEqual([2.4, 3.2])
+    expect(move([0, 0], [3, 4], 1)).toEqual([0.6, 0.8])
   })
 })
